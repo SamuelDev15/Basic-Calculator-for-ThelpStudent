302ab1aeada6c0b7e7d42cefa01b3cd1293da094
liinadev <Linalizethcruzs@gmail.com>
2025-05-16 16:52:53 -0500

    se agregan funciones y estilos

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Proyecto Calculadora 
 ## Descripción
-- Proyecto de calculadora funcional, aprender a programar funciones basicas de una calculadora dando indicaciones de como usarla.
+- Proyecto de calculadora funcional, la cual tiene programadas funciones basicas dando indicaciones de como usarla.
 - Funciones Basicas(Sumar-Restar-Multiplicar-Dividir)
 - Funcion borrar
+- funcion borrar último 
 - Cuenta con validaciones de cero y reconocimiento de errores
 ### Tecnologies
 - Html
--- index.js
+++ index.js
@@ -1,24 +1,34 @@
+
 const calculadora = {
-    type : "casio",
-    modelo : "2010",
-    color : "blue",
-    digitos : 8,
-    pantallaInteractiva : true,
-    
-     sumar:function (a, b) {
-      return(a + b);
-    },
-     restar:function (a, b) {
-      return(a - b); 
-    },
-     multiplicar:function (a, b) {
-      return(a * b);  
-    },
-     dividir:function (a, b) {
-      return(a / b);  
-    },
-    addNumber:function (number){
-    document.getElementById("display").value = number;
-    },  
-    
+  type : "casio",
+  modelo : "2010",
+  color : "rgb(180, 111, 54)"
 }
+
+function calculadoraAgregar(valor) {
+  const display = document.getElementById("display");
+  display.value += valor;
+}
+
+function borrar() {
+  const display = document.getElementById("display");
+  display.value = ""
+}
+
+function borrarUltimo() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calcular() {
+  const input = document.getElementById("display").value;
+  const display = document.getElementById("display");
+  try {
+    // Crea una nueva función que devuelve el resultado de la expresión
+    const result = Function('"use strict"; return (' + input + ')')();
+    display.value = result;
+  } catch {
+    display.value = "Error";
+  }
+}
+
